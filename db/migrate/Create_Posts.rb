31c3425a7693e951afb8fb664a0eb8b765e8c03d
Christian_Polanco <chrispolanco@sbcglobal.net>
2019-10-05 16:27:29 -0700

    added details to tables

--- db/migrate/Create_Posts.rb
+++ db/migrate/Create_Posts.rb
@@ -1,4 +1,13 @@
 class CreatePosts < ActiveRecord::Migration
+  
   def change
+    create_table :posts do |t|
+      t.string  :title 
+      t.string  :link 
+      t.text    :description 
+      t.float   :latitude 
+      t.float   :longitude
+      t.integer :user_id 
+    end 
   end
 end
