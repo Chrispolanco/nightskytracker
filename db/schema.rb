b8664e02fe70a3fc30df320aa144d0c854f2be95
Christian_Polanco <chrispolanco@sbcglobal.net>
2019-10-05 21:33:53 -0700

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string  "title"
+    t.string  "link"
+    t.text    "description"
+    t.float   "latitude"
+    t.float   "longitude"
+    t.integer "user_id"
+    t.date    "date"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "password"
+  end
 
 end
