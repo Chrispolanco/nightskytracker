dbe19d509c57636d6999726f9be9f1d072fada18
Christian_Polanco <chrispolanco@sbcglobal.net>
2019-10-06 10:30:27 -0700

    added first helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,22 @@ class ApplicationController < Sinatra::Base
     enable :sessions
     set :session_secret, "password_security"
   end
+  
   get '/' do
      "Start The Party"
   end
+
+  helpers do 
+
+    def logged_in?
+      !!session[user_id]
+    end  
+
+    def current_user
+      User.find(session[:user_id])
+    end 
+
+  end 
+
 end
 
