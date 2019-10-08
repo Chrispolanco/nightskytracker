b3e057eb52f846b01fa2d048c0ed936979fe0749
Christian_Polanco <chrispolanco@sbcglobal.net>
2019-10-07 22:21:08 -0700

    updated post edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,12 @@
 class PostsController < ApplicationController
 
+    get "/posts" do 
+        @posts = Post.all 
+        erb :"/posts/index"
+    end 
+
+    get "/posts/new" do
+        erb :"/posts/new"
+    end
+
 end 
