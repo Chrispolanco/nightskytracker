e44ac7574c66e8f25238c5f6312059fa846cdbe6
Christian_Polanco <chrispolanco@sbcglobal.net>
2019-10-10 23:14:52 -0700

    cleaned up code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,36 +13,6 @@ class UsersController < ApplicationController
         end
     end 
 
-    get "/users/login" do 
-        erb :"users/login" 
-    end 
-
-    post "/users/login" do 
-        user = User.find_by(:username => params[:username]) 
-        if user && user.authenticate(params[:password]) 
-            sesstion[:user_id] = user.id
-            redirect "users/success" 
-        else 
-            redirect "users/failure"
-        end 
-    end 
-
-    get "/users/success" do 
-        if logged_in
-            erb :"users/success"
-        else 
-            redirect "users/login"
-        end 
-    end 
-
-    get "/users/failure" do 
-        erb :"users/failure" 
-    end 
-
-    get "/users/logout" do 
-        session.clear
-        redirect "/"
-    end 
 
 end 
         
